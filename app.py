7e36ae8f2778a7cc2090e49742264933272705ad
Vipul Surwase <80000167+vipulsurw@users.noreply.github.com>
2025-06-03 15:45:44 +0000

    corrected app.py

--- app.py
+++ app.py
@@ -40,10 +40,6 @@ freezer = Freezer(app)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 if __name__ == '__main__':
     freezer.freeze()
 
