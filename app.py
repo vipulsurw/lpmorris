c40e6392bbac87f280561b5d8108e8fd84c6e970
Vipul Surwase <80000167+vipulsurw@users.noreply.github.com>
2025-06-03 15:35:39 +0000

    frozen-flask installed and app.py changed

--- app.py
+++ app.py
@@ -18,10 +18,32 @@ if __name__ == '__main__':
     app.run(debug=True)
 """
 
-
+"""
 @app.route('/')
 def home():
     return 'Hello, Replit!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
+"""
+
+# after frozen-flask
+
+from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    freezer.freeze()
+
