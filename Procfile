ce0d6cd68d1c4f4e37262de509653c7babdeb9c7
Vipul Surwase <80000167+vipulsurw@users.noreply.github.com>
2025-06-03 13:13:46 +0000

    Initial Flask Setup

---
+++ Procfile
@@ -0,0 +1,3 @@
+# Procfile
+web: gunicorn app:app
+
---
+++ app.py
@@ -0,0 +1,27 @@
+# app.py
+from flask import Flask, render_template
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+"""
+unquote if workaround below doesn't work
+# Define a route for the home page ('/')
+@app.route('/')
+def home():
+    # Render the 'index.html' template when the home page is accessed
+    return render_template('index.html')
+
+# This block ensures the Flask development server runs only when the script is executed directly
+if __name__ == '__main__':
+    # Run the Flask application in debug mode (useful for development, automatically reloads on code changes)
+    app.run(debug=True)
+"""
+
+
+@app.route('/')
+def home():
+    return 'Hello, Replit!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
